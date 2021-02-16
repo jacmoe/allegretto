7b5dfdd4670bd071e55951eaa7d5f32e2e923d47
Jacob Moen <jacmoe.dk@gmail.com>
2021-02-16 17:11:57 +0100

    Smart pointers for Allegro objects

--- Allegretto/code/main.cpp
+++ Allegretto/code/main.cpp
@@ -1,48 +1,118 @@
+#include <stdio.h>
+#include <memory>
+
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
-#include <stdbool.h>
+#include <allegro5/allegro_image.h>
 
-int main()
+#include "utility/ALDeleter.hpp"
+
+int main(int argc, char** argv)
 {
-    al_init();
-    al_install_keyboard();
+    if (!al_init())
+    {
+        printf("couldn't initialize allegro\n");
+        return 1;
+    }
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(320, 200);
-    ALLEGRO_FONT* font = al_create_builtin_font();
+    if (!al_install_keyboard())
+    {
+        printf("couldn't initialize keyboard\n");
+        return 1;
+    }
 
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
+    std::unique_ptr<ALLEGRO_TIMER, utility::ALDeleter> timer;
+    std::unique_ptr<ALLEGRO_EVENT_QUEUE, utility::ALDeleter> queue;
+    std::unique_ptr<ALLEGRO_DISPLAY, utility::ALDeleter> disp;
+    std::unique_ptr<ALLEGRO_FONT, utility::ALDeleter> font;
+    std::unique_ptr<ALLEGRO_BITMAP, utility::ALDeleter> mysha;
 
+    timer.reset(al_create_timer(1.0 / 30.0));
+    if (!timer.get())
+    {
+        printf("couldn't initialize timer\n");
+        return 1;
+    }
+
+    queue.reset(al_create_event_queue());
+    if (!queue.get())
+    {
+        printf("couldn't initialize queue\n");
+        return 1;
+    }
+
+    disp.reset(al_create_display(640, 480));
+    if (!disp.get())
+    {
+        printf("couldn't initialize display\n");
+        return 1;
+    }
+
+    font.reset(al_create_builtin_font());
+    if (!font.get())
+    {
+        printf("couldn't initialize font\n");
+        return 1;
+    }
+
+    if (!al_init_image_addon())
+    {
+        printf("couldn't initialize image addon\n");
+        return 1;
+    }
+
+    mysha.reset(al_load_bitmap("assets/textures/mysha.png"));
+    if (!mysha.get())
+    {
+        printf("couldn't load mysha\n");
+        return 1;
+    }
+
+    al_register_event_source(queue.get(), al_get_keyboard_event_source());
+    al_register_event_source(queue.get(), al_get_display_event_source(disp.get()));
+    al_register_event_source(queue.get(), al_get_timer_event_source(timer.get()));
+
+    bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
 
-    al_start_timer(timer);
+    al_start_timer(timer.get());
     while (1)
     {
-        al_wait_for_event(queue, &event);
+        al_wait_for_event(queue.get(), &event);
 
-        if (event.type == ALLEGRO_EVENT_TIMER)
+        switch (event.type)
+        {
+        case ALLEGRO_EVENT_TIMER:
+            // game logic goes here.
             redraw = true;
-        else if ((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
             break;
 
-        if (redraw && al_is_event_queue_empty(queue))
+        case ALLEGRO_EVENT_KEY_DOWN:
+        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+            done = true;
+            break;
+        }
+
+        if (done)
+            break;
+
+        if (redraw && al_is_event_queue_empty(queue.get()))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_text(font.get(), al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_bitmap(mysha.get(), 100, 100, 0);
             al_flip_display();
 
             redraw = false;
         }
     }
 
-    al_destroy_font(font);
-    al_destroy_display(disp);
-    al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
+    //al_destroy_bitmap(mysha);
+    //al_destroy_font(font);
+    //al_destroy_display(disp);
+    //al_destroy_timer(timer);
+    //al_destroy_event_queue(queue);
 
     return 0;
 }
---
+++ Allegretto/code/utility/ALDeleter.hpp
@@ -0,0 +1,36 @@
+/*# This file is part of the
+# ██████╗ ██╗  ██╗██╗     ██╗    ██╗ ██████╗ ██╗     ███████╗
+# ██╔══██╗╚██╗██╔╝██║     ██║    ██║██╔═══██╗██║     ██╔════╝
+# ██████╔╝ ╚███╔╝ ██║     ██║ █╗ ██║██║   ██║██║     █████╗  
+# ██╔═══╝  ██╔██╗ ██║     ██║███╗██║██║   ██║██║     ██╔══╝  
+# ██║     ██╔╝ ██╗███████╗╚███╔███╔╝╚██████╔╝███████╗██║     
+# ╚═╝     ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝     
+#   project
+#
+#   https://github.com/jacmoe/pxlwolf
+#
+#   (c) 2020 - 2021 Jacob Moena
+#
+#   MIT License
+#*/
+#pragma once
+/*
+* Coded by Balint Kiss for his Raycaster-engine project at https://github.com/balintkissdev/raycaster-engine
+* Copyright (c) 2012-2021 Balint Kiss - MIT licensed.
+* Modified slightly by Jacob Moena (added the TTF_Font)
+*/
+
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+
+namespace utility {
+    struct ALDeleter
+    {
+        void operator()(ALLEGRO_DISPLAY* display) { al_destroy_display(display); }
+        void operator()(ALLEGRO_TIMER* timer) { al_destroy_timer(timer); }
+        void operator()(ALLEGRO_EVENT_QUEUE* eventqueue) { al_destroy_event_queue(eventqueue); }
+        void operator()(ALLEGRO_FONT* font) { al_destroy_font(font); }
+        void operator()(ALLEGRO_BITMAP* bitmap) { al_destroy_bitmap(bitmap); }
+    };
+}
